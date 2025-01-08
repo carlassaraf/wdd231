9a7845467023cfb02caeb6218a64c741a1dcac89
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2025-01-08 18:39:45 -0300

    feat: course certificates creation and styling

--- scripts/index.js
+++ scripts/index.js
@@ -4,3 +4,21 @@ document.querySelector("#currentyear").innerText = date.getFullYear();
 
 // Last modified
 document.querySelector("#lastModified").innerText = document.lastModified;
+
+// Dynamic rendering of courses
+
+const container = document.querySelector("#courses");
+
+// Create a new element for each course
+
+courses.forEach(course => {
+
+    const div = document.createElement("div");
+    div.setAttribute("class", "course");
+    div.innerText = `${course.subject} ${course.number}`;
+    if(course.completed) {
+        div.classList.add("completed");
+    }
+
+    container.append(div);
+});
