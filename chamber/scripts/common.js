c26f260c4bd05af9b9718e0489aafffc64b5c640
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2025-01-16 03:27:21 -0300

    feat: year and last modified

---
+++ chamber/scripts/common.js
@@ -0,0 +1,6 @@
+// Current year
+const date = new Date();
+document.querySelector("#currentyear").innerText = date.getFullYear();
+
+// Last modified
+document.querySelector("#lastModified").innerText = document.lastModified;
